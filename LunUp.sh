de29c22ebfba23a14451da1e1de13d54526e2cc2
Simone <37718696+Simo190@users.noreply.github.com>
2019-05-10 18:15:23 +0200

    Create LunUp.sh

---
+++ LunUp.sh
@@ -0,0 +1,20 @@
+wget https://github.com/LunariumCoin/lunarium/releases/download/v1.2.0/lunarium-1.2.0-x86_64-linux-gnu.tar.gz
+tar xvzf lunarium-1.2.0-x86_64-linux-gnu.tar.gz
+cd lunarium-1.2.0/bin/
+chmod +x lunarium-cli
+chmod +x lunariumd
+cd bin
+lunarium-cli stop
+sleep 30
+killall -15 lunariumd
+rm /usr/local/bin/lunarium-cli
+rm /usr/local/bin/lunariumd
+mv lunarium-cli /usr/local/bin/
+mv lunariumd /usr/local/bin/
+lunariumd -daemon 
+rm lunarium-1.2.0-x86_64-linux-gnu.tar.gz
+sleep 20
+lunarium-cli -version
+sleep 20
+watch lunarium-cli masternode status
+rm -r lunarium-1.2.0
